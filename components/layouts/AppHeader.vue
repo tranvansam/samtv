f1adc061c953fa411b978089facefca500cf2017
tranvansam <transamdhkh@gmail.com>
2025-09-29 23:23:54 +0700

    fix menu

--- components/layouts/AppHeader.vue
+++ components/layouts/AppHeader.vue
@@ -134,6 +134,23 @@
                 <span>{{ item.label }}</span>
               </div>
             </a>
+            <NuxtLink 
+              v-else
+              :to="item.path"
+              @click="scrollToTopAndCloseMenu"
+              :class="[
+                item.mobileClass,
+                'block px-4 py-3 text-white hover:text-orange-400 hover:bg-white/5 rounded-lg transition-all duration-300',
+                isActiveRoute(item.path) ? '!text-orange-400' : ''
+              ]"
+            >
+              <div class="flex items-center space-x-2">
+                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                </svg>
+                <span>{{ item.label }}</span>
+              </div>
+            </NuxtLink>
           </li>
           
 
